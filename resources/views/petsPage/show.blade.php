e61bdb5667e722827a0cd3b525c12ba0f195859e
Dijana <didimx.18@gmail.com>
2022-10-21 15:55:07 +0200

    last

--- resources/views/petsPage/show.blade.php
+++ resources/views/petsPage/show.blade.php
@@ -8,24 +8,25 @@
    
 </style>
    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+<div class="img">
+               <img src="/images/pets/{{ $animal->image->path }}" alt="">
+              </div>
 
-
-        
-           <div> 
-              <ul>
-                 <li>Name:{{$animal->name}}</li>
-                 <li>Species:{{$animal->species}}</li>
-                <li> Owner: {{$animal->owner->first_name." ".$animal->owner->surname}}</li>
-                <li>Breed:{{$animal->breed}}</li>
-                <li> Weight in pounds: {{$animal->weight}}</li>
-                <li> Age: {{$animal->age}}</li>
-
+<div> 
+                 <ul class="pet__container" >
+               <li>Name: {{$animal->name}}</li>
+               <li>Species: {{$animal->species}}</li>
+               <li>Breed: {{$animal->breed}}</li>
+               <li> Weight in pounds: {{$animal->weight}}</li>
+               <li> Age: {{$animal->age}}</li>
+               <br>
+               <li> Owner: {{$animal->owner->first_name." ".$animal->owner->surname}}</li>
               </ul>
-                
-             
+               <form action="{{Route('pets.edit',  ['id' => $animal->id])}}">
+                  <button>EDIT</button>
+               </form>
           </div>
-       
 
-    
+   
 </div>
 @endsection
